ebed07449b50de5a9c59431800bb8dbec9c55f78
Jaffer <alijaffer3000@gmail.com>
2018-07-17 23:34:43 -0400

    scroll updates

--- js/main.js
+++ js/main.js
@@ -9,36 +9,6 @@ requestAnimationFrame(animate);
 
 var leaving = false; 
 function regTopBar() {
-//    console.log(document.body.scrollTop)//document.documentElement.scrollTop
-    if(document.body.scrollTop >= 100 && leaving == false){
-        leaving = true
-        console.log("leave")
-        let pos = {x: 160, y: 0}
-        let tween = new TWEEN.Tween(pos)
-            .to({x: 90, y: 0}, 400)
-            .easing(TWEEN.Easing.Quadratic.InOut)
-            .onUpdate(function(){
-                //$("#topbar").css('transform', 'translateY(' + pos.x + 'px)');
-                $("#topbar").css('height', (pos.x + 'px'));
-                $("#mainlogo").css('height', ((Math.max(70, pos.x - 60) + 'px')))
-
-            }).start()
-        
-    }
-    else if(document.body.scrollTop < 100 && leaving == true){
-        leaving = false
-        let pos = {x: 90, y: 0}
-        let tween = new TWEEN.Tween(pos)
-                .to({x: 160, y: 0}, 400)
-                .easing(TWEEN.Easing.Quadratic.InOut)
-                .onUpdate(function(){
-                    //$("#topbar").css('transform', 'translateY(' + pos.x + 'px)');
-                    $("#topbar").css('height', (pos.x + 'px'));
-                    console.log(pos.x)
-                    $("#mainlogo").css('height', (Math.min((pos.x - 20),100) + 'px'))
-                }).start()
-        console.log("enter")
-    }
 }
 
 function mouseEnteredTopBar(event) {
@@ -66,7 +36,36 @@ function mouseLeaveTopBar(event) {
 //
 window.onload = function(){
     console.log("load")
-    $(window).scroll(function() {console.log("scroll") regTopBar()});
+    $(window).scroll(function() {console.log("scroll") regTopBar()
+//    console.log(document.body.scrollTop)//document.documentElement.scrollTop
+        if(document.body.scrollTop >= 100 && leaving == false){
+            leaving = true
+            console.log("leave")
+            let pos = {x: 160, y: 0}
+            let tween = new TWEEN.Tween(pos)
+                .to({x: 90, y: 0}, 400)
+                .easing(TWEEN.Easing.Quadratic.InOut)
+                .onUpdate(function(){
+                    //$("#topbar").css('transform', 'translateY(' + pos.x + 'px)');
+                    $("#topbar").css('height', (pos.x + 'px'));
+                    $("#mainlogo").css('height', ((Math.max(70, pos.x - 60) + 'px')))
+                }).start()
+        }
+        else if(document.body.scrollTop < 100 && leaving == true){
+            leaving = false
+            let pos = {x: 90, y: 0}
+            let tween = new TWEEN.Tween(pos)
+                    .to({x: 160, y: 0}, 400)
+                    .easing(TWEEN.Easing.Quadratic.InOut)
+                    .onUpdate(function(){
+                        //$("#topbar").css('transform', 'translateY(' + pos.x + 'px)');
+                        $("#topbar").css('height', (pos.x + 'px'));
+                        console.log(pos.x)
+                        $("#mainlogo").css('height', (Math.min((pos.x - 20),100) + 'px'))
+                    }).start()
+            console.log("enter")
+        }
+    });
     document.getElementById("topbar").onmouseenter = function() {mouseEnteredTopBar()};
     document.getElementById("topbar").onmouseleave = function() {mouseLeaveTopBar()};
 }
