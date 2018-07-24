d442056df75ac0cf798a9bef4e35cda81d6213ef
Jaffer <alijaffer3000@gmail.com>
2018-07-24 16:39:01 -0400

    oof

--- js/article.js
+++ js/article.js
@@ -1,8 +1,5 @@
-//i'm sorry for this code
+---
+---
 console.log($('#getmonth'))
 
-
-
-$.get("/_data/contributors.json", function(data){
-    console.log(data)  
-})
+var chapters = {{site.data.articles}};
